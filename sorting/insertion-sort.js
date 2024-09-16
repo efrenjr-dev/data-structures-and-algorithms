4c18fa03d6ffc07fcefb3bfa9a86d3f6b9d91401
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-09-16 21:26:20 +0800

    insertion and merge sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,16 @@
+const insertionSort = (array) => {
+    for (let i = 1; i < array.length; i++) {
+        let current = array[i];
+        let j = i - 1;
+        while (j >= 0 && current < array[j]) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = current;
+    }
+    return array;
+};
+
+const myArray = [4, 2, 6, 5, 1, 3];
+const result = insertionSort(myArray);
+console.log(result);
---
+++ sorting/merge-sort.js
@@ -0,0 +1,38 @@
+const merge = (left, right) => {
+    const result = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    result.push(...left.slice(i));
+    result.push(...right.slice(j));
+
+    return result;
+};
+
+const mergeSort = (array) => {
+    if (array.length <= 1) return array;
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+    console.log("left", left);
+    console.log("right", right);
+    return merge(mergeSort(left), mergeSort(right));
+};
+
+// const myArray1 = [4, 21, 36, 80, 92];
+// const myArray2 = [2, 13, 25, 42, 90];
+// const result = merge(myArray1, myArray2);
+// console.log(result);
+
+const unsortedArray = [4, 21, 2, 13, 25, 42, 9, 36, 80, 92];
+const sortedArray = mergeSort(unsortedArray);
+console.log(sortedArray);
