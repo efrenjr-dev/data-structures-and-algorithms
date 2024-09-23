53865894a032bb9b18b830adde7c7a14a956d5d8
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-09-23 08:08:07 +0800

    challenges

--- arrays/palindrome.js
+++ arrays/palindrome.js
@@ -6,4 +6,33 @@ const isPalindrome = (word) => {
     return word === reverseString(word);
 };
 
-console.log(isPalindrome("abcba"));
+const isPalindromeSentence = (s) => {
+    function isSpecialCharacter(char) {
+        const regex = /^[a-zA-Z0-9]$/;
+        return !regex.test(char);
+    }
+
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+        while (l < r && isSpecialCharacter(s[l])) {
+            l++;
+        }
+        while (l < r && isSpecialCharacter(s[r])) {
+            r--;
+        }
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+            console.log("l", s[l], "r", s[r]);
+            return false;
+        }
+        l++;
+        r--;
+    }
+
+    return true;
+};
+
+// console.log(isPalindrome("abcba"));
+// console.log(isPalindromeSentence("Was it a car or a cat I saw?"));
+console.log(isPalindromeSentence(".,"));
